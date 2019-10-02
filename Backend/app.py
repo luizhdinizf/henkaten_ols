3bd89f5e3fe5d542b66a4a5306eaed63f841615c
Luiz Henrique <luizhdinizf@gmail.com>
2019-10-02 00:53:39 -0300

    envio da image via http backend e frontend rasp

--- Backend/app.py
+++ Backend/app.py
@@ -1,13 +1,19 @@
 import time
-
-from flask import Flask
+import cv2
+import jsonpickle
+import numpy as np
+import random
+from flask import Flask, render_template, flash, request,Response
 from mongoCli import *
 import json
 import sys
+import numpy as np
+
 
 
 app = Flask(__name__)
-
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = 'SjdnUends821Jsdlkvxh391ksdODnejdDw'
 
 
 def get_hit_count():
@@ -19,7 +25,23 @@ def get_hit_count():
         except:
             return 5
 
+@app.route('/api/upload', methods=['POST'])
+def test():
+    r = request
+    # convert string of image data to uint8
+    nparr = np.fromstring(r.data, np.uint8)
+    # decode image
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    cv2.imwrite('img.jpg',img)
+    # do some fancy processing here....
 
+    # build a response dict to send back to client
+    response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])
+                }
+    # encode response using jsonpickle
+    response_pickled = jsonpickle.encode(response)
+
+    return Response(response=response_pickled, status=200, mimetype="application/json")
 @app.route('/')
 def hello():
     count = get_hit_count()
